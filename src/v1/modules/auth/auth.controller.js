1d663566597032ce8c1aa45e4eeb5fb4cefe872e
avanibaheti2 <avani@gochoo.in>
2024-09-03 14:21:38 +0530

    integrated Msg91 service to send otp via SMS

--- src/v1/modules/auth/auth.controller.js
+++ src/v1/modules/auth/auth.controller.js
@@ -1,24 +1,66 @@
+import axios from 'axios';
+import FCM from 'fcm-node';
 import User from '../../models/user.js';
 import { generateOTP, isOTPExpired } from '../../../utils/otp.js';
-import { createJWT, verifyToken } from '../../../utils/token.js'; // Import the token functions
+import { createJWT, verifyToken } from '../../../utils/token.js';
 import StatusCodes from 'http-status-codes';
 import httpFormatter from '../../../utils/formatter.js';
 import rateLimit from 'express-rate-limit';
-import dotenv from 'dotenv'; 
+import dotenv from 'dotenv';
+import { FCM_KEY } from '../../../utils/constants.js';
 
-dotenv.config(); 
+dotenv.config();
 
 const otpLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, 
-    max: 5, 
+    windowMs: 5 * 60 * 1000,
+    max: 5,
     message: 'Too many OTP requests from this IP, please try again in 5 minutes.',
     keyGenerator: (req) => req.body.phoneNumber,
 });
 
-// Function for handling signup
+const fcm = new FCM(FCM_KEY);
+
+const messageAuthKey = process.env.MESSAGE_AUTH_KEY;
+const messageTemplate = process.env.MESSAGE_TEMPLATE;
+
+const prependCountryCode = (phoneNumber) => {
+    return `91${phoneNumber}`;
+};
+
+const sendMessage = async (otp, mobile) => {
+    const options = {
+        method: 'POST',
+        url: 'https://control.msg91.com/api/v5/flow',
+        headers: {
+            authkey: messageAuthKey,
+            accept: 'application/json',
+            'content-type': 'application/json'
+        },
+        data: {
+            template_id: messageTemplate,
+            short_url: "1",
+            realTimeResponse: "1",
+            recipients: [
+                {
+                    mobiles: mobile,
+                    OTP: otp
+                }
+            ]
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log('Message sent:', response.data);
+    } catch (error) {
+        console.error('Error sending message:', error);
+    }
+};
+
 export const signup = async (req, res) => {
     try {
-        const { phoneNumber, otp } = req.body;
+        let { phoneNumber, otp } = req.body;
+        phoneNumber = prependCountryCode(phoneNumber);
 
         if (!phoneNumber) {
             return res.status(StatusCodes.BAD_REQUEST).json(httpFormatter({}, 'Phone number is required', false));
@@ -28,7 +70,6 @@ export const signup = async (req, res) => {
             let user = await User.findOne({ phoneNumber });
 
             if (!otp) {
-                // Generate OTP for signup
                 if (user && user.verified) {
                     return res.status(StatusCodes.BAD_REQUEST).json(httpFormatter({ user }, 'User already registered', false));
                 }
@@ -37,27 +78,28 @@ export const signup = async (req, res) => {
                     user = await User.create({
                         phoneNumber,
                         otp: generateOTP(),
-                        otpExpires: new Date(Date.now() + 5 * 60 * 1000),
+                        otpExpires: new Date(Date.now() + 5 * 60 * 1000), // OTP expires in 5 minutes
                         otpRequestCount: 1,
                         lastOtpRequest: new Date(),
                     });
                 } else {
                     user.otp = generateOTP();
-                    user.otpExpires = new Date(Date.now() + 5 * 60 * 1000);
+                    user.otpExpires = new Date(Date.now() + 5 * 60 * 1000); // OTP expires in 5 minutes
                     user.otpRequestCount = (user.otpRequestCount || 0) + 1;
                     user.lastOtpRequest = new Date();
                 }
 
                 await user.save();
                 console.log(`OTP for signing up user with ${phoneNumber}: ${user.otp}`);
+
+                await sendMessage(user.otp, phoneNumber);
+
                 return res.status(StatusCodes.OK).json(httpFormatter({ user }, 'OTP sent successfully. Please verify.', true));
             } else {
-                // Verify OTP for signup
                 if (!user || user.otp !== otp || isOTPExpired(user.otpExpires)) {
                     return res.status(StatusCodes.BAD_REQUEST).json(httpFormatter({}, 'Invalid or expired OTP', false));
                 }
 
-                // OTP is valid, complete signup
                 user.verified = true;
                 user.otp = undefined;
                 user.otpExpires = undefined;
@@ -77,10 +119,10 @@ export const signup = async (req, res) => {
     }
 };
 
-// Function for handling login
 export const signin = async (req, res) => {
     try {
-        const { phoneNumber, otp } = req.body;
+        let { phoneNumber, otp } = req.body;
+        phoneNumber = prependCountryCode(phoneNumber);
 
         if (!phoneNumber) {
             return res.status(StatusCodes.BAD_REQUEST).json(httpFormatter({}, 'Phone number is required', false));
@@ -90,26 +132,27 @@ export const signin = async (req, res) => {
             let user = await User.findOne({ phoneNumber });
 
             if (!otp) {
-                // Generate OTP for login
                 if (!user) {
                     return res.status(StatusCodes.NOT_FOUND).json(httpFormatter({}, 'User not found', false));
                 }
 
                 user.otp = generateOTP();
-                user.otpExpires = new Date(Date.now() + 5 * 60 * 1000);
+                user.otpExpires = new Date(Date.now() + 5 * 60 * 1000); // OTP expires in 5 minutes
                 user.otpRequestCount = (user.otpRequestCount || 0) + 1;
                 user.lastOtpRequest = new Date();
                 await user.save();
 
                 console.log(`OTP for logging in user with ${phoneNumber}: ${user.otp}`);
+
+                await sendMessage(user.otp, phoneNumber);
+
                 return res.status(StatusCodes.OK).json(httpFormatter({ user }, 'OTP sent successfully. Please verify.', true));
             } else {
-                // Verify OTP for login
                 if (!user || user.otp !== otp || isOTPExpired(user.otpExpires)) {
                     return res.status(StatusCodes.BAD_REQUEST).json(httpFormatter({}, 'Invalid or expired OTP', false));
                 }
 
-                user.isLoggedIn = true; // Only set to true if OTP is valid
+                user.isLoggedIn = true;
                 user.otp = undefined;
                 user.otpExpires = undefined;
                 await user.save();
@@ -128,10 +171,8 @@ export const signin = async (req, res) => {
     }
 };
 
-// Function for handling logout
 export const logout = async (req, res) => {
     try {
-        // Use the verifyToken middleware to handle token verification
         verifyToken(req, res, async () => {
             const userId = req.user.userId;
 
