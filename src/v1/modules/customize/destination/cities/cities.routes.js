d8c1f452cbb56fab31def29631569f2de1f5b8bd
avanibaheti2 <avani@gochoo.in>
2024-09-05 11:20:46 +0530

    removed remove-activity function

--- src/v1/modules/customize/destination/cities/cities.routes.js
+++ src/v1/modules/customize/destination/cities/cities.routes.js
@@ -6,6 +6,5 @@ const router = express.Router();
 router.post('/cities', addCity); 
 router.get('/cities', getAllCities); 
 router.post('/cities/add-activity', addActivityToCity); 
-router.post('/cities/remove-activity', removeActivityFromCity); 
 
 export default router;
