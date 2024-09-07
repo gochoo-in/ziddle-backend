3f15ab7b13cb303af49dc1ec02d9acaf4e9920b1
avanibaheti2 <avani@gochoo.in>
2024-09-07 12:49:54 +0530

    ADD route for getting all users (only admin can access)

--- src/v1/modules/admin/user.controller.js
+++ src/v1/modules/admin/user.controller.js
@@ -34,3 +34,24 @@ export const updateUserRole = async (req, res, next) => {
     );
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(StatusCodes.NOT_FOUND).json(
+        httpFormatter({}, 'No users found.', false)
+      );
+    }
+
+    return res.status(StatusCodes.OK).json(
+      httpFormatter({ users }, 'Users retrieved successfully.', true)
+    );
+  } catch (error) {
+    console.error('Error retrieving users:', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+      httpFormatter({}, 'Error retrieving users.', false)
+    );
+  }
+};
--- src/v1/modules/admin/user.routes.js
+++ src/v1/modules/admin/user.routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { updateUserRole } from './user.controller.js';
+import { updateUserRole, getAllUsers } from './user.controller.js';
 import isAdmin from '../../../utils/middleware.js';
 
 const router = express.Router();
 router.patch('/:userId', isAdmin, updateUserRole);
+router.get('/users', isAdmin, getAllUsers);
+
 export default router;
