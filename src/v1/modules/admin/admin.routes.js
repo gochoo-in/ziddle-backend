4b299c99f52bb9950a283898feeb21e15beab666
avanibaheti2 <avani@gochoo.in>
2024-09-14 17:00:43 +0530

    chore: added casbinMiddleware in admin signup

--- src/v1/modules/admin/admin.routes.js
+++ src/v1/modules/admin/admin.routes.js
@@ -4,7 +4,7 @@ import { verifyToken } from '../../../utils/token.js';
 import { casbinMiddleware } from '../../../utils/casbinMiddleware.js'
 const router = express.Router();
 
-router.post('/signup', adminSignup);
+router.post('/signup', casbinMiddleware, adminSignup);
 router.post('/signin', adminSignin);
 router.post('/logout', verifyToken, adminLogout);
 router.delete('/:employeeId', casbinMiddleware, deleteEmployee)
