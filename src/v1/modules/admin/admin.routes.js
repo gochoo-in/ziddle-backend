6afa3a769acce471eb9d56a65058f3ad93be0f76
avanibaheti2 <avani@gochoo.in>
2024-09-18 15:13:58 +0530

    ADD get route for getting all employees

--- src/v1/modules/admin/admin.routes.js
+++ src/v1/modules/admin/admin.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { adminSignup, adminSignin, adminLogout, deleteEmployee } from './admin.controller.js';
+import { adminSignup, adminSignin, adminLogout, deleteEmployee, getAllEmployees } from './admin.controller.js';
 import { verifyToken } from '../../../utils/token.js';
 import { casbinMiddleware } from '../../../utils/casbinMiddleware.js'
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/signup', casbinMiddleware, adminSignup);
 router.post('/signin', adminSignin);
 router.post('/logout', verifyToken, adminLogout);
 router.delete('/:employeeId', casbinMiddleware, deleteEmployee)
+router.get('/employees', getAllEmployees);
 
 export default router;
