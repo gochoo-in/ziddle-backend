20f7d036f7939e6801dad4822e6ecefa1050ec6f
amritgo <amrit@gochoo.in>
2024-09-26 11:28:46 +0530

    chore: added images in activity and city schema

--- src/v1/models/city.js
+++ src/v1/models/city.js
@@ -5,6 +5,19 @@ const CitySchema = new mongoose.Schema({
   name: { type: String, required: true },
   iataCode: { type: String, required: true },
   destination: { type: mongoose.Schema.Types.ObjectId, ref: 'Destination', required: true },
+  imageUrls: {
+    type: [{
+        type: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }],
+    default: []
+},
   country: { type: String, required: true },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
