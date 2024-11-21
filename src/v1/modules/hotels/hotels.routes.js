362ec4b9a993442c70f1e5a187178b7300621739
avanibaheti2 <avani@gochoo.in>
2024-11-21 17:05:49 +0530

    added route get cities from country code

--- src/v1/modules/hotels/hotels.routes.js
+++ src/v1/modules/hotels/hotels.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getTopHotels,getSpecificHotelDetails } from './hotels.controller.js';
+import { getTopHotels,getSpecificHotelDetails, getCityName } from './hotels.controller.js';
 
 const router = express.Router();
 
 router.get('/:cityId/hotels', getTopHotels);
 
 router.get('/:cityId/hotels/:hotelCode',getSpecificHotelDetails)
+
+router.post('/hotelCityNames', getCityName)
 
 export default router;
