1dac2499f0beea5380891f8582850333eda1db8c
avanibaheti2 <avani@gochoo.in>
2024-12-05 15:22:14 +0530

    hot fix: currently added otp as 1111 instead of dynamic otp

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,6 +1,7 @@
 export const generateOTP = () => {
-    const otp = Math.floor(1000 + Math.random() * 9000); 
-    return otp.toString();
+    // const otp = Math.floor(1000 + Math.random() * 9000); 
+    // return otp.toString();
+    return '1111'
 };
 
 export const isOTPExpired = (expiryDate) => {
